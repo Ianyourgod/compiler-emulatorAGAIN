31d9abd5b91d2847ac57be31f98f95b5c5a9f643
Ianyourgod <ianlouishawthorne@gmail.com>
2023-01-18 23:09:49 -0600

    first commit

---
+++ emulator.py
@@ -0,0 +1,43 @@
+from os import system
+class Emulator:
+    def __init__(self) -> None:
+        self.createBank()
+        self.registers = [0,0,0,0,0,0,0,0]
+        self.updateScreen()
+    def clearConsole(self):
+        system("clear" if system == "nt" else "cls")
+    def clearScreen(self):
+        for i in range(1024):
+            self.bank[15][i] = 0
+    def createBank(self) -> None:
+        self.bank = []
+        for i in range(16):
+            self.bank.append([])
+            for a in range(pow(2, 16)):
+                self.bank[i].append(0)
+    def load(self, program: str) -> None:
+        program = program.split("\n")
+        for i in range(len(program)):
+            program[i] = program[i].split(" ")
+        for i in range(len(program)):
+            for a in range(len(program[i])):
+                program[i][a] = int(program[i][a])
+
+    def updateScreen(self):
+        self.clearConsole()
+        printing = []
+        for i in range(1024):
+            if i % 63 == 0:
+                printing.append("")
+            printing[i//64] += chr(self.bank[15][i]) if self.bank[15][i] != 0 else " "
+        print("_" * 64)
+        printing.pop()
+        for i in printing:
+            print("|" + i + "|")
+        print("-" * 64)
+
+
+em = Emulator()
+while True:
+    em.updateScreen()
+    input()
---
+++ readme.md
@@ -0,0 +1,13 @@
+# Wierd Language
+Welcome to Ianyourgod's wierd language!
+This is a compiled language, which can then be run on the custom emulator built in with the language.
+## Computer part
+Built in computer B)
+### The Emulator
+This is a simple emulator. It is not necessarily copying anything, more just an adaption of what I believe the computer I would build would be like.
+### The Assembly
+As stated above, this is a custom emulator. This means I will also have a custom assembly language. This assembly language is a bit higher level than the byte code, but still pretty low level.
+### The Bytecode
+This will probably not be explained well, as I will not be writing docs for it and if I do they wil be pretty simple. This is because who tf is going to be directly writing the bytecode.
+## The Actual Language
+TODO
